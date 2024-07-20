4b3235896021c3a0a8a5102c25c1c5a601d6b161
7SIX975 <lkhf459@gmail.com>
2024-07-20 16:53:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    autoplay: {
-        delay: 3000, // เปลี่ยนสไลด์ทุก 3 วินาที
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
 AOS.init({
     duration: 1000,
     once: true
